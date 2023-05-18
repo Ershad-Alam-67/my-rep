e8e340aba809c1984710538c0784bd7c848a4dea
Ershad-Alam-67 <ealam6732@gmail.com>
2023-05-18 15:29:20 +0530

    Hi there

---
+++ paran.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean} {()}[]
+ */
+var isValid = function (s) {
+  let pair = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+  let ar = [];
+  for (let i of s) {
+    if (i === "(" || i === "{" || i === "[") {
+      ar.push(i);
+    }
+    if (i === ")" || i === "}" || i === "]") {
+      if (pair[i] === ar.pop()) {
+        continue;
+      } else {
+        return false;
+      }
+    }
+  }
+  if (ar.length === 0) {
+    return true;
+  }
+  return false;
+};
+console.log(isValid("{()}[]"));
